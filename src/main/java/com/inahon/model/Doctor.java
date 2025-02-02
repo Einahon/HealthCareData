5639219bc42da6c5f592f5310fe292b98d6e1bc9
elie <inahon.elie17@yahoo.com>
2025-02-02 14:05:38 -0500

    Created Doctor entity in feature/model

---
+++ src/main/java/com/inahon/model/Doctor.java
@@ -0,0 +1,79 @@
+package com.inahon.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Doctors")
+public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Doctor_ID")
+    private int doctorId;
+    @Column(name = "FirstName")
+    private String firstName;
+    @Column(name = "LastName")
+    private String lastName;
+    @Column(name = "Specialty")
+    private String speciality;
+    @Column(name = "Email")
+    private String email;
+
+    public Doctor() {}
+    public Doctor(String firstName, String lastName, String speciality, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.speciality = speciality;
+        this.email = email;
+    }
+
+    public int getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(int doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "doctorId=" + doctorId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", speciality='" + speciality + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
